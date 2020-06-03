e6e8d037b1067a5f7f5a77ce017a4f3373b73f8a
TiSAngelo <tisangelo@gmail.com>
2020-06-03 18:56:59 -0300

    Dia 03 aplicação web concluída até o select da UF

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Routes from './routes';
+
+//JSX: permite que se escreva com a sintaxe do XML(html) dentro do JavaScript.
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World</h1>
-      </header>
-    </div>
+  return (   
+    <Routes />
   );
 }
 
